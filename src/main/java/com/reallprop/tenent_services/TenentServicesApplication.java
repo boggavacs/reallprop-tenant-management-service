95335a7b8a4cdf67b149b5c9ca0ca4353fb55dec
Shekar B <boggavarpucs@outlook.com>
2026-06-15 18:02:49 -0400

    initial commit

---
+++ src/main/java/com/reallprop/tenent_services/TenentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.reallprop.tenent_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenentServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reallprop/tenent_services/TenentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reallprop.tenent_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenentServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
